396dc1a98e7aff597404b925002437ad23b1b2eb
Noah Wilderom <61543656+Noah-Wilderom@users.noreply.github.com>
2022-02-16 15:05:28 +0100

    Update Controller.cs

--- Controller.cs
+++ Controller.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +10,13 @@ namespace AdventureGame
 {
     class Controller
     {
+
+        public Controller()
+        {
+            Console.WriteLine("Controller class init");
+            Console.ReadKey();
+            Console.Clear();
+            
+        }
     }
 }
